0eb936f0714c1c8a5cc4368f9ba7d71bfdb69a90
Yanick Reis <127902779+yanickreisn@users.noreply.github.com>
2025-11-18 18:53:11 -0300

    Create aluno.java

---
+++ src/aluno.java
@@ -0,0 +1,23 @@
+public class Aluno {
+    private int id;
+    private String nome;
+    private String email;
+
+    public Aluno(int id, String nome, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public int getId() { return id; }
+    public String getNome() { return nome; }
+    public String getEmail() { return email; }
+
+    public void setNome(String nome) { this.nome = nome; }
+    public void setEmail(String email) { this.email = email; }
+
+    @Override
+    public String toString() {
+        return "Aluno: " + nome + " (ID " + id + ")";
+    }
+}
